e3bc2685726e357615df991b7a74f12dbee98f23
Peijing Teh <pteh@c1r1s3.42singapore.sg>
2023-08-01 18:36:33 +0800

    completed ex02

--- ex02/ft_str_is_alpha.c
+++ ex02/ft_str_is_alpha.c
@@ -6,24 +6,34 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/01 14:54:06 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/01 15:14:07 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/01 18:23:31 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_str_is_alpha(char *str)
 {
+	int	char_counter;
+	int	str_counter;
+
+	char_counter = 0;
+	str_counter = 0;
 	while (*str != '\0')
 	{
-		//compare pointer deref to each value in string literal 
 		if (((int)*str > 64 && (int)*str < 91)
 			|| ((int)*str > 96 && (int)*str < 123)
-			|| ((int)str == 0))
+			|| ((int)*str == 0))
 		{
-			return('1');
-		} else
+			char_counter++;
+		}
+		else
 		{
-			return('0');
+			char_counter = 0;
 		}
 		str++;
+		str_counter++;
 	}
+	if (char_counter == str_counter)
+		return (1);
+	else
+		return (0);
 }
--- testmain02.c
+++ testmain02.c
@@ -1,23 +1,43 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   test_main.c                                        :+:      :+:    :+:   */
+/*   testmain02.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 15:37:58 by pteh              #+#    #+#             */
-/*   Updated: 2023/07/31 15:39:36 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/01 18:35:06 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
+int	ft_str_is_alpha(char *st);
 
 int	main()
 {
 	//include function to test here.
 
+	char *source = "Helloooooooooo"; 
+	char *wrong_start = "_Helloooooooooo"; 
+	char *wrong_mid = "_Hell*ooooooooo"; 
+	char *wrong_end = "_Hellooooooooo%"; 
+  
+	int a = ft_str_is_alpha(source);
+	int b = ft_str_is_alpha(wrong_start);
+	int c = ft_str_is_alpha(wrong_mid);
+	int d = ft_str_is_alpha(wrong_end);
 
-	//
-	//
+	printf("Source: %s\n", source);
+	printf("Source: %s\n", wrong_start);
+	printf("Source: %s\n", wrong_mid);
+	printf("Source: %s\n", wrong_end);
+
+	printf("Result: %d\n", a);
+	printf("Result: %d\n", b);
+	printf("Result: %d\n", c);
+	printf("Result: %d\n", d);
+return 0;
+
 }
