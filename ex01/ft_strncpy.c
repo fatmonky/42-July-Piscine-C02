c4eae7ec9a0d8de4ff50544f64d7815ae67817d1
PJ Teh <peijing.teh.dev@gmail.com>
2023-08-02 21:24:04 +0800

    updated logic: not complete. Need to figure out how to fill up dest with null values when src less than n.

--- ex01/ft_strncpy.c
+++ ex01/ft_strncpy.c
@@ -20,22 +20,20 @@ char	*ft_strncpy(char *dest, char *src, unsigned int n)
 
 	counter = 0;
 	original_dest = dest;
-
-	//while 
-
 	while ((*src != '\0') && (counter <= n -1))
 	{ 
 		*dest = *src;
 		dest++;
 		src++;
 		counter++;
-		if (counter < n)//to amend tomorrow 3 Aug.
-
-
+		if ((counter < n) && (*src == '\0'))
+				while (counter <= n - 1)
+					{
+					*dest = '\0';
+					dest++; //this leads to segmentation fault. Need a counter here to help me track.
+					counter++; // this counter leads to a counter that fills up dest at most to n. how to fill up rest of dest with null?
+				}
 	}
-	if (counter < n)
-		
-	*dest = *src;
 	return (original_dest);
 }
 
@@ -48,13 +46,21 @@ int	main(void)
 	ft_strncpy(destination, source, 5);
 	printf("Source: %s\n", source);
 	printf("Destination: %s\n", destination);
+	strncpy(destination, source, 5);
+	printf("StrncpyDestination: %s\n", destination);
+
 	source = "Hell";
 	ft_strncpy(destination, source, 5);
 	printf("Source: %s\n", source);
 	printf("Destination: %s\n", destination);
+	strncpy(destination, source, 5);
+	printf("StrncpyDestination: %s\n", destination);
+
 	source = "He\0ll";
 	ft_strncpy(destination, source, 5);
 	printf("Source: %s\n", source);
 	printf("Destination: %s\n", destination);
+	strncpy(destination, source, 5);
+	printf("StrncpyDestination: %s\n", destination);
 	return (0);
 }
