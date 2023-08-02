cb99e2022345b218f61000b6b6c90a854f86a169
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-02 11:03:03 +0800

    refactored ex06 a la norminette.

--- ex06/ft_str_is_printable.c
+++ ex06/ft_str_is_printable.c
@@ -6,7 +6,7 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 10:02:32 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/02 10:52:58 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/02 11:01:54 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,19 +24,16 @@ int	check_contains(char *str)
 
 int	ft_str_is_printable(char *str)
 {
-
-	while (*str) //check str's value not '\0', indicating end of the sentence. 
-		{//for each str value in string, feed *str into check_contains function.
-		 //if check_contains returns true,
-		 //move pointer str to next value in string i.e. str++;
+	while (*str)
+	{
 		if (check_contains(str) == 1)
-			{
+		{
 			str++;
-			} else
-			{
-				return (0);
-			}
 		}
-// return (1) if pointer at end of string.
-return (1);
+		else
+		{
+			return (0);
+		}
+	}
+	return (1);
 }
