c365f7ca7de1c9013a0c4a6ddd68a41f70d949a6
Peijing Teh <pteh@c1r1s2.42singapore.sg>
2023-08-03 10:12:26 +0800

    reworked ex00, 01, 07, 08.

--- ex08/ft_strlowcase.c
+++ ex08/ft_strlowcase.c
@@ -6,9 +6,12 @@
 /*   By: pteh <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/02 13:14:33 by pteh              #+#    #+#             */
-/*   Updated: 2023/08/02 13:21:55 by pteh             ###   ########.fr       */
+/*   Updated: 2023/08/03 10:11:08 by pteh             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+/*#include <unistd.h>
+#include <string.h>
+#include <stdio.h>*/
 
 int	check_contains(char *str)
 {
@@ -28,3 +31,32 @@ char	*ft_strlowcase(char *str)
 	}
 	return (str);
 }
+/*
+int	main(void)
+{
+	char source[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+	char wrong_start[] = "asdfkM987*-`zasdf%@#$%ABCDEFGHIJKLMNOPQRSTUVWXYZ"; 
+	char wrong_mid[] = "MABCDEFGHIJK987*-`zasdf%@#$%LMNOPQRSTUVWXYZ"; 
+	char wrong_end[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZM987*-`zasdf%@#$%asdfqwer"; 
+	char empty[]= ""; 
+
+	printf("Source: %s\n", source);
+	printf("wrong_start: %s\n", wrong_start);
+	printf("wrong_mid: %s\n", wrong_mid);
+	printf("wrong_end: %s\n", wrong_end);
+	printf("empty: %s\n", empty);
+
+	ft_strlowcase(source);
+	ft_strlowcase(wrong_start);
+	ft_strlowcase(wrong_mid);
+	ft_strlowcase(wrong_end);
+	ft_strlowcase(empty);
+
+	printf("trans: %s\n", source);
+	printf("transwrong_start: %s\n", wrong_start);
+	printf("transwrong_mid: %s\n", wrong_mid);
+	printf("transwrong_end: %s\n", wrong_end);
+	printf("transempty: %s\n", empty);
+
+return (0);
+}*/
